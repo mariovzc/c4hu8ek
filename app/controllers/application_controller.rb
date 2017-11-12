d41bc3b732e8f9419b4596a17e2ea92fdf3d231d
¨Mario <mario.vizcainos@gmail.com>
2017-11-11 22:59:35 -0500

    setup the user auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+
+
+  def authenticate
+    email = request.headers['X-User-Email']
+    api_token = request.headers['X-Api-Token']
+    @user = User.find_by(email: email, api_token: api_token)
+
+    unless @user
+      head status: :unauthorized
+      return false
+    end
+  end
 end
